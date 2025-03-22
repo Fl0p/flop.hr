dd5a72f804e1f55c8cdfe3b2d2145ad183dd546c
Flop <flop@hackerspace.by>
2025-03-22 10:52:28 +0100

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-between p-8">
+      <main className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </main>
+      <footer className="text-center text-gray-500 text-sm py-4">
+        Under Development
+      </footer>
+    </div>
+  );
+}
